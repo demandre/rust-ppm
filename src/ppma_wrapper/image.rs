73f751f1d1d010a4dcf621623a1754e264626338
azerty <jinw96@gmail.com>
2020-01-24 10:59:14 +0100

    Begin doc

--- src/ppma_wrapper/image.rs
+++ src/ppma_wrapper/image.rs
@@ -1,22 +1,42 @@
+#![crate_name = "doc"]
+
 extern crate libc;
 use libc::c_int;
 
 extern crate itertools;
 use itertools::izip;
 
-#[derive(Clone, Default, Debug)]
-struct Pixel {
+/// RGB pixel struct : 
+/// * r : byte for red
+/// * g : byte for green
+/// * b : byte for blue
+#[derive(Clone, Default, Debug, Copy)]
+pub struct Pixel {
     r: u8,
     g: u8,
     b: u8,
 }
 
 impl Pixel {
-    pub fn new() -> Pixel {
+    /// Returns a Pixel with r, g and b bytes given
+    ///
+    ///  # Arguments
+    ///
+    ///  * `r` : red byte
+    ///  * `g` : green byte
+    ///  * `b` : blue byte
+    ///
+    ///  # Example
+    ///
+    ///  ```
+    ///  use image::Pixel
+    ///  let pixel = Pixel::new(10, 50, 200);
+    ///  ```
+    pub fn new(r : u8, g : u8, b : u8) -> Pixel {
         Pixel {
-            r : 0,
-            g : 0,
-            b : 0
+            r,
+            g,
+            b
         }
     }
 }
@@ -32,11 +52,11 @@ impl Image {
     pub fn from_r_g_b(r : Vec<c_int>, g : Vec<c_int>, b : Vec<c_int>, width : u32, height : u32) -> Image {
         let mut pixel_vec : Vec<Pixel> = Vec::new();
         for (r_elem, g_elem, b_elem) in izip!(&r, &g, &b) {
-            pixel_vec.push(Pixel {
-                r : *r_elem as u8,
-                g : *g_elem as u8,
-                b : *b_elem as u8
-            });
+            pixel_vec.push(Pixel::new(
+                *r_elem as u8,
+                *g_elem as u8,
+                *b_elem as u8
+            ));
         }
         Image {
             width,
