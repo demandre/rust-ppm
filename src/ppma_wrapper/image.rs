fdeda7885b2570688e2a4eea58a22f13ca7ad37e
azerty <jinw96@gmail.com>
2020-02-13 18:32:03 +0100

    Add doc for image.rs

--- src/ppma_wrapper/image.rs
+++ src/ppma_wrapper/image.rs
@@ -1,5 +1,3 @@
-#![crate_name = "doc"]
-
 extern crate libc;
 use libc::c_int;
 
@@ -50,6 +48,10 @@ pub trait Grayscalable {
     fn grayscale_luma(&mut self);
 }
 
+/// Image struct :
+/// * width: image width
+/// * height: image height
+/// * content: image pixels
 #[derive(Clone, Default, Debug)]
 pub struct Image {
     pub width: u32,
@@ -58,6 +60,24 @@ pub struct Image {
 }
 
 impl Image {
+    /// Convert r,g,b vectors into an Image struct
+    /// 
+    /// # Arguments:
+    /// * `r` : red byte vector
+    /// * `g` : green byte vector
+    /// * `b` : blue byte vector
+    /// * `width` : number of pixels per line
+    /// * `height` : number of lines
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use image::Image;
+    /// let r = vec![0, 1, 2, 3];
+    /// let g = vec![0, 1, 2, 3];
+    /// let b = vec![0, 1, 2, 3];
+    /// let image = Image::from_r_g_b(r, g, b, 2, 2);
+    /// ```
     pub fn from_r_g_b(r : Vec<c_int>, g : Vec<c_int>, b : Vec<c_int>, width : u32, height : u32) -> Image {
         let mut pixel_vec : Vec<Pixel> = Vec::new();
         for (r_elem, g_elem, b_elem) in izip!(&r, &g, &b) {
@@ -74,6 +94,22 @@ impl Image {
         }
     }
 
+    /// Convert Image struct to r, g, b vectors
+    ///
+    /// # Arguments:
+    /// * `image` : borrowed pointer to the image struct
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use image::{Image, Pixel};
+    /// let image = {
+    ///     width: 1,
+    ///     height: 1,
+    ///     content: vec![Pixel::new(0, 0, 0)]
+    /// };
+    /// let (r, g, b) = Image::to_r_g_b(&image);
+    /// ```
     pub fn to_r_g_b(image : &Image) -> (Vec<c_int>, Vec<c_int>, Vec<c_int>) {
         let mut r : Vec<c_int> = Vec::new();
         let mut g : Vec<c_int> = Vec::new();
@@ -88,6 +124,19 @@ impl Image {
 }
 
 impl Invertable for Image {
+    /// Invert colors of an image
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use image::{Image, Pixel};
+    /// let mut image = {
+    ///     width: 1,
+    ///     height: 1,
+    ///     content: vec![Pixel::new(0, 0, 0)]
+    /// };
+    /// image.invert();
+    /// ```
     fn invert(&mut self) {
         for mut pixel in &mut self.content {
             pixel.r = 255-pixel.r;
@@ -98,6 +147,19 @@ impl Invertable for Image {
 }
 
 impl Grayscalable for Image {
+    /// Grayscale an image
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use image::{Image, Pixel};
+    /// let mut image = {
+    ///     width: 1,
+    ///     height: 1,
+    ///     content: vec![Pixel::new(0, 0, 0)]
+    /// }
+    /// image.grayscale();
+    /// ```
     fn grayscale(&mut self) {
         for mut pixel in &mut self.content {
             let average = pixel.r/3 + pixel.g/3 + pixel.b/3;
