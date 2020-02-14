45e485d9dc1f63a8e62197d6452f467ca7b7837d
Joris Demandre <joris.demandre@agence-soon.fr>
2020-02-14 06:56:21 +0100

    add back cli and use test library if no args

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,10 @@ extern crate clap;
 mod ppma_wrapper;
 use ppma_wrapper::image::Invertable;
 use ppma_wrapper::image::Grayscalable;
-
-
 use clap::{Arg, App};
 
-
 fn main() { 
-    /*let matches = App::new("ppm")
+    let matches = App::new("ppm")
        .version("1.0")
        .about("PPM utility")
        .author("Demandre J.")
@@ -26,24 +23,19 @@ fn main() {
     if input != None {
         println!("{}",input.unwrap());
     } else {
-        // Test Pixel struct
-        let pixel = Pixel { r: 42, ..Default::default() };
+        testLibraries();
+    }
+}
 
-        println!("{:?}",pixel);
+fn testLibraries() {
+    let mut image = ppma_wrapper::create_example_ppm_wrapper(1000, 1000);
+    ppma_wrapper::ppma_write_wrapper(String::from("test.ppm"), image.clone());
 
-        // Test Image struct
-        let pixel2 = Pixel { r: 43, ..Default::default() };
-        let mut image = Image { width:242, content: vec![], ..Default::default() };
-        image.content.push(pixel);
-        image.content.push(pixel2);
-
-        println!("{:?}",image);
-    }*/
-    let mut image_test_to_invert = ppma_wrapper::create_example_ppm_wrapper(10, 10);
+    let mut image_test_to_invert = image.clone();
     image_test_to_invert.invert();
     ppma_wrapper::ppma_write_wrapper(String::from("testInverted.ppm"), image_test_to_invert);
 
-    let mut image_test_to_grayscale = ppma_wrapper::create_example_ppm_wrapper(10, 10);
+    let mut image_test_to_grayscale = image.clone();
     image_test_to_grayscale.grayscale_luma();
     ppma_wrapper::ppma_write_wrapper(String::from("testGreyscaled.ppm"), image_test_to_grayscale);
 }
